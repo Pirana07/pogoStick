d9ef4eb4273c9af62eeb318100c3d12934447994
Pirana07 <pirananika58@gmail.com>
2024-08-06 01:21:28 +0400

    s

--- Assets/Scripts/PogoStickController.cs
+++ Assets/Scripts/PogoStickController.cs
@@ -2,65 +2,88 @@ using UnityEngine;
 
 public class PogoStickController : MonoBehaviour
 {
+    // საჯარო ველი გადახტომის ძალისთვის
     [SerializeField]  float jumpForce = 10f;
+    // პირადი სერალიზებული ველები
     [SerializeField]  float _rotationSpeed = 5f;
     [SerializeField]  Transform _groundCheck;
     [SerializeField]  float _groundCheckRadius = 0.2f;
     [SerializeField]  LayerMask _groundLayer;
-    [SerializeField]  ParticleSystem _groundParticles; // მიწაზე მოხვედრის ნაწილაკები
+    [SerializeField]  ParticleSystem _groundImpactParticles; // მიწაზე შეჯახების ნაწილაკები
 
      Rigidbody2D _rb;
      bool _isGrounded;
-     bool _wasGrounded; // წინა მიწაზე ყოფნის მდგომარეობა
 
     void Start()
     {
+        // Rigidbody2D კომპონენტის მოპოვება, რომელიც მიბმულია game object-ზე
         _rb = GetComponent<Rigidbody2D>();
+
+        // დარწმუნდით, რომ ნაწილაკთა სისტემა თავდაპირველად არ არის აქტიური
+        if (_groundImpactParticles != null)
+        {
+            _groundImpactParticles.Stop();
+            _groundImpactParticles.Clear();
+        }
     }
 
     void Update()
     {
+        // პოგოს ჯოხის მაუსის კურსორისკენ მობრუნება
         RotateTowardsMouse();
+        // შეამოწმეთ, შეეხო თუ არა პოგოს ჯოხი მიწას
         CheckGround();
 
-        if (_isGrounded && !_wasGrounded)
-        {
-            PlayGroundParticles(); // მიწაზე მოხვედრის ნაწილაკების გაშვება
-        }
-
+        // თუ პოგოს ჯოხი მიწაზეა, გადახტეთ
         if (_isGrounded)
         {
             Jump();
         }
-
-        _wasGrounded = _isGrounded; // განახლება წინა მიწაზე ყოფნის მდგომარეობის
     }
 
     void Jump()
     {
+        // გამოთვალეთ გადახტომის მიმართულება და გამოიყენეთ სიჩქარე
         Vector2 jumpDirection = transform.up;
         _rb.velocity = new Vector2(jumpDirection.x * jumpForce, jumpDirection.y * jumpForce);
     }
 
     void RotateTowardsMouse()
     {
+        // მაუსის პოზიციის მიღება მსოფლიო კოორდინატებში
         Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        // გამოთვალეთ მიმართულება პოგოს ჯოხიდან მაუსის პოზიციისკენ
         Vector2 direction = (mousePosition - transform.position).normalized;
+        // გამოთვალეთ მობრუნების კუთხე
         float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg - 90f;
+        // შექმენით როტაცია გამოთვლილი კუთხისკენ
         Quaternion targetRotation = Quaternion.Euler(new Vector3(0, 0, angle));
+        // გლუვად მობრუნდით მიზნის როტაციისკენ
         transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, _rotationSpeed * Time.deltaTime);
     }
 
     void CheckGround()
     {
+        // შეამოწმეთ, შეეხო თუ არა პოგოს ჯოხი მიწას
+        bool wasGrounded = _isGrounded;
         _isGrounded = Physics2D.OverlapCircle(_groundCheck.position, _groundCheckRadius, _groundLayer);
+
+        // თუ პოგოს ჯოხი ახლახან დაეშვა, გამოისახეთ მიწის შეჯახების ნაწილაკები
+        if (_isGrounded && !wasGrounded)
+        {
+            SpawnGroundImpactParticles();
+        }
     }
 
-    void PlayGroundParticles()
+    void SpawnGroundImpactParticles()
     {
-        if (_groundParticles != null)
+        // თუ მიწის შეჯახების ნაწილაკები კომპონენტი მინიჭებულია
+        if (_groundImpactParticles != null)
         {
-            _groundParticles.Play(); // ნაწილაკების გაშვება
+            // ნაწილაკთა სისტემის გადაადგილება მიწის შემოწმების პოზიციაზე
+            _groundImpactParticles.transform.position = _groundCheck.position;
+            // ნაწილაკთა სისტემის ჩართვა
+            _groundImpactParticles.Play();
         }
     }
 }
